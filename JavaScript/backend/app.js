847b1a56c0693519d08d3ae4d782af9b335617b9
SambaranBanerjee <sanbaranb25@gmail.com>
2025-12-08 18:18:32 +0530

    Added a functionality for sendFile

--- JavaScript/backend/app.js
+++ JavaScript/backend/app.js
@@ -1,9 +1,16 @@
 import express from 'express';
+import { join } from 'path';
 
 const app = express();
 const port = 3000;
 
-app.get("/", (req,res) => {
+app.get("/", (req, res) => {
+    const fullPath = join("C:/Users/samba/Documents/Git_Personal/Full-Stack-Practice",'JavaScript', 'frontend' , 'index.html');
+    console.log(join("C:/Users/samba/Documents/Git_Personal/Full-Stack-Practice",'JavaScript', 'frontend', 'index.html'));
+    res.sendFile(fullPath);
+})
+
+app.get("/health", (req,res) => {
     try {
         res.send("The server says hello!");
         console.log("Successfully entered the server.");
