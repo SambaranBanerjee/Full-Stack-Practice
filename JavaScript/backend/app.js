9e4ddce44ce8fd67ab862136568864f962b16046
SambaranBanerjee <sanbaranb25@gmail.com>
2025-12-09 09:30:07 +0530

    Created JSON files, added .env, added route for request parameters

--- JavaScript/backend/app.js
+++ JavaScript/backend/app.js
@@ -1,13 +1,25 @@
 import express from 'express';
-import { join } from 'path';
+import path, { join } from 'path';
+import dotenv from 'dotenv';
 
 const app = express();
-const port = 3000;
+dotenv.config();
+const port = process.env.PORT || 3000;
+console.log(process.env.PORT);
 
 app.get("/", (req, res) => {
     const fullPath = join("C:/Users/samba/Documents/Git_Personal/Full-Stack-Practice",'JavaScript', 'frontend' , 'index.html');
     console.log(join("C:/Users/samba/Documents/Git_Personal/Full-Stack-Practice",'JavaScript', 'frontend', 'index.html'));
     res.sendFile(fullPath);
+})
+
+app.get("/posts", (req,res) => {
+    res.sendFile(join("C:/Users/samba/Documents/Git_Personal/Full-Stack-Practice", "JavaScript", "backend", "data.json"));
+})
+
+app.get("/posts/:id", (req, res) => {
+    console.log(req.params);
+    res.sendFile(join("C:/Users/samba/Documents/Git_Personal/Full-Stack-Practice", "JavaScript", "backend", "data.json"));
 })
 
 app.get("/health", (req,res) => {
@@ -23,4 +35,7 @@ app.get("/health", (req,res) => {
 
 app.listen(port, () => {
     console.log(`The server is listening on PORT ${port}`);
+}).on("error", (error) => {
+    console.log('Server failed to execute', error);
+    process.exit(1);
 })
