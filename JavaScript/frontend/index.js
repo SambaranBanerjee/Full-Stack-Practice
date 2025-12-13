b5816031fad016b0045392293d67a52f23e8e39a
SambaranBanerjee <sanbaranb25@gmail.com>
2025-12-13 08:19:27 +0530

    Added the POST sending function from frontend

--- JavaScript/frontend/index.js
+++ JavaScript/frontend/index.js
@@ -1,5 +1,44 @@
-document.getElementById("btn-1").addEventListener("click", () => {
-    const name = document.getElementById("name").value;
-    const age = document.getElementById("age").value;
-    return alert(`The user ${name} is of age ${age}`);
+document.getElementById("btn-1").addEventListener("click", (e) => {
+    e.preventDefault();
+    
+    const nameInput = document.getElementById('name');
+    const nameValue = nameInput.value;
+    const ageInput = document.getElementById('age');
+    const ageValue = Number(ageInput.value);
+
+    const postData = {
+        name: nameValue,
+        age: ageValue
+    };
+
+    if (nameValue.trim() === '' || ageValue === '') {
+        alert("Please enter all input values");
+        return;
+    }
+    sendPosts(postData);
+    
 });
+
+async function sendPosts(postData) {
+    try {
+        const response = await fetch('http://localhost:8080/posts', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(postData),
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        console.log('Post created: ', data);
+        alert('Data successfully submitted!'); 
+
+    } catch (error) {
+        console.error('Error in posting data: ', error);
+        alert('Failed to connect to the server or post data.');
+    }
+}
